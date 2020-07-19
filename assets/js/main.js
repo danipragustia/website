b6df9beb3aa55d09d582fe68c2f6567396db6d85
danipragustia <danipragustia@disroot.org>
2020-07-19 14:09:15 +0700

    optimize: make code more reasonable

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,1 @@
-let btnSize = document.getElementById('btnFontsize')
-
-btnSize.addEventListener('click', (event) => {
-	if (document.body.style.fontSize == "") { document.body.style.fontSize = '1em'; }
-	if (btnSize.getAttribute('value') == 'Normal') {
-		btnSize.setAttribute('value') == 'Big Size'
-		btnSize.innerHTML('Big Size')
-		document.body.style.fontSize = parseFloat(document.body.style.fontSize) + (1 * 0.3) + 'em'
-	} else {
-		btnSize.setAttribute('value') == 'Normal'
-		btnSize.innerHTML('Normal Size')
-		document.body.style.fontSize = '1em'
-	}
-})
+document.getElementById('btnFontsize').addEventListener('click', (event) => document.body.style.fontSize = (document.body.style.fontSize == '1em' || document.body.style.fontSize == '' ? parseFloat(document.body.style.fontSize) + (1 * 0.3) : '1') + 'em')
