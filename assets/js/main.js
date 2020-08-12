d48cd88f0fc0d718d41caea0c07a513c6c4ea2fe
Dani Pragustia <danipragustia@disroot.org>
2020-08-12 11:22:50 +0700

    update: fix it!

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,1 @@
-document.getElementById('btnFontsize').addEventListener('click', (event) => document.body.style.fontSize = (document.body.style.fontSize == '1em' || document.body.style.fontSize == '' ? parseFloat(document.body.style.fontSize) + (1 * 0.3) : '1') + 'em')
+document.getElementById('btnFontsize').addEventListener('click', (event) => document.body.style.fontSize = (document.body.style.fontSize == '1em' || '' ? parseFloat(document.body.style.fontSize) + (1 * 0.3) : '1') + 'em')
--- lograth-jekyll.gemspec
+++ lograth-jekyll.gemspec
@@ -14,9 +14,11 @@ Gem::Specification.new do |spec|
 
   spec.add_runtime_dependency "jekyll", "~> 3.6"
   spec.add_runtime_dependency "jekyll-seo-tag", "~> 2.1"
-
+  
   spec.add_development_dependency "jekyll-feed", "~> 0.9"
+  spec.add_development_dependency "kramdown-parser-gfm", "~> 1.1"
   spec.add_development_dependency "jekyll-paginate", "~> 1.1"
   spec.add_development_dependency "jekyll-redirect-from", "~> 0.12"
   spec.add_development_dependency "bundler", "~> 2.0"
+
 end
